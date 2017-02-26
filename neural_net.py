94411f8fc3ec122d070f68a96b49ab419bdea3ee
Javier <javierloplom@gmail.com>
2017-02-26 15:45:21 -0400

    entrenando y funcionando pero solo con arquitectura 2,3,1

--- neural_net.py
+++ neural_net.py
@@ -4,40 +4,48 @@ import numpy as np
 # Seed para inicialización al azar para las matrices en las redes neurales
 np.random.seed(42+5)
 
+# np.seterr(all='warn')
+# np.seterr( over='ignore' )
+
 # Función sigmoidal y versión vectorizada
-def sigmoid(value):
-    return 1 / (1+np.exp(-value))
+def sigmoid(value,derivative=False):
+    value = np.clip( value, -500, 500 )
+
+    sig = 1.0 / (1+np.exp(-value))
+
+    if derivative:
+        return np.multiply(sig, 1-sig)
+    else:
+        return sig
 vsigmoid = np.vectorize(sigmoid)
 
 # Gradiente para función sigmoidal
-def sigmoid_grad(value):
-    return sigmoid(value) * (1 - sigmoid(value))
+# def sigmoid_grad(value):
+#     return sigmoid(value) * (1 - sigmoid(value))
+# vsigmoid_grad = np.vectorize(sigmoid_grad)
 
 class NeuralNetwork():
     def __init__(self,layer_list):
         self.matrices  = []
         self.layers    = layer_list
 
-        for r,c in zip(layer_list[:-1],layer_list[1:]):
+        for c,r in zip(layer_list[:-1],layer_list[1:]):
             # Deberíamos romper la posible simetría de la matriz que agrega el random
-            self.matrices.append(np.matrix(np.random.random((r+1,c))))
+            self.matrices.append(np.matrix(np.random.random((r,c+1))))
 
     def forward_prop(self,inp):
-        activations = []
+        z = []
+        a = []
         for layer in self.matrices:
             # Multiplicar, aplicar función logística y repetir en siguiente capa
             ones_vect   = np.matrix(np.ones( (inp.shape[0],1) ))
             inp         = np.append(ones_vect,inp,axis=1)
-            # print(inp.shape)
-            # print(layer.shape)
-            # print("---------")
-            inp         = vsigmoid(inp*layer)
-            # print(type(inp))
-            # print("appending")
-            activations += [inp]
-            # print(activations)
+            a           += [inp]
+            inp         = (layer*inp.T).T
+            z += [inp]
+            inp         = vsigmoid(inp)
 
-        return (inp,activations)
+        return (inp,z,a)
 
     def predict(self,inp,treshold=0.5):
         val = self.forward_prop(inp)[0]
@@ -55,46 +63,54 @@ class NeuralNetwork():
         return J
         # J /= m;
 
-    def back_propagate(self,X,y,iterations=4,alpha=0.5):
-        m           = len(self.matrices)
-        delta = [ np.zeros(i.shape) for i in self.matrices]
+    def back_propagate(self,X,y,iterations=100,alpha=0.1):
+        m     = len(self.matrices)
+        delta = [ 0 for i in self.matrices]
+        costs = []
         
         for i in range(iterations):
             # From last to first layer
             print(self.get_cost(X,y) )
-            activations = self.forward_prop(X)[1]
-            last_error  = [activations[-1]-y]
-            activations = activations[:-1]
-            activations.reverse()
-            for i,a in enumerate(activations):
+            costs += [self.get_cost(X,y)]
+            final,activation,pre_act = self.forward_prop(X)
+            last_error  = [final-y]
+            # activations = activations[:-1]
+            # activations.reverse()
+            # import pdb; pdb.set_trace()
+
+            for i in reversed(range(m-1)):
                 # print(a)
-                sig = np.matrix(np.multiply(a,(1-a)))
+                # sig = np.matrix(np.multiply(activation[i],(1-activation[i])))
+                sig = vsigmoid(pre_act[i],True)
+                # sig = np.matrix(np.multiply(activations[i],(1-activations[i])))
                 # print(self.matrices[m-i-1].shape)
                 # print(last_error[i].shape)
-                print(i)
-                print(m-i-1)
-                err = last_error[i]*self.matrices[m-i-1].T
-                last_error += [err.T * sig  ]
-                # import pdb; pdb.set_trace()
+                # print(i)
+                # print(m-i-1)
+                
+                # aux_mat = np.matrix(np.copy(self.matrices[1]))
+                # aux_mat = aux_mat
+                err = last_error[-1] * np.delete(self.matrices[i+1],0,1) 
+                last_error += [ np.multiply(err,sig)  ]
 
             # print("UPDATE")
             last_error.reverse()
             
             # import pdb; pdb.set_trace()
-            self.matrices[-1] -= sum(np.multiply(last_error[-1],self.matrices[-1].T)).T * alpha
+            # self.matrices[-1] -= sum(np.multiply(last_error[-1],self.matrices[-1].T)).T * alpha
 
-            for i,e in enumerate(last_error[:-1]):
-                print("ENTREEE")
-                # print(self.matrices[i].shape)
-                # print(last_error[i].shape)
-                # print(activations[i].shape)
-                # last_error[i] 
-                # delta[i] = delta[i] +  activations[i].T * last_error[i]
-                self.matrices[i] -= sum(last_error[i] * self.matrices[i]) * alpha
-                # self.matrices[i] = self.matrices[i] -  delta[i] * alpha
+            for i in range(m):
+                delta[i] = (pre_act[i].T * last_error[i]).T / y.shape[0]
 
-            # for i in range(len(last_error)-1):
-            #     self.matrices[i] = self.matrices[i] -  delta[i] * alpha
+            for i in range(m):
+                # import pdb; pdb.set_trace()
+                self.matrices[i] -= alpha * delta[i]
+
+        import matplotlib.pyplot as plt
+        # import pdb; pdb.set_trace()
+
+        plt.plot(range(iterations), costs)
+        plt.show()
             
                 
 
@@ -109,7 +125,26 @@ class NeuralNetwork():
 net = NeuralNetwork([2,3,1])
 # print(net)
 # print(net.forward_prop(np.matrix([[2,4],[1,3]])))
-data_500 = np.matrix(np.loadtxt('datos_P2_EM2017_N500.txt'))
+data_500 = np.matrix(np.loadtxt('datos_P2_EM2017_N500.txt'),dtype=np.float128)
+
+
+
+def normalizacion(matrix,mean=None,std=None,columns=None):
+    if mean is None:
+        mean = matrix.mean(0)
+        std  = matrix.std(0)
+    
+    if not columns:
+        columns = range(0,matrix.shape[1])
+    for i in columns:
+        if std[i] != 0:
+            matrix[:,i] = (matrix[:,i] - mean[i]) / std[i]
+
+    return mean,std
+
+# import pdb; pdb.set_trace()
+
+# data_500 = data_500 - data_500.mean(0) / data_500.std(0)
 
 X = data_500[:,:-1]
 y = data_500[:,-1]
